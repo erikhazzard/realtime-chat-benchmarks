24cac5546e549c6c7ef7d412b14c9def9c51acbe
Erik Hazzard <enoex1@gmail.com>
2014-07-02 15:05:17 -0700

    updates WS tests

--- websockets/ws/server.js
+++ websockets/ws/server.js
@@ -1,16 +1,38 @@
 var WebSocketServer = require('ws').Server, 
     wsServer = new WebSocketServer({port: 3000});
+var colors = require('colors');
 
+console.log("\t\t\tWS Server starting".bold.blue);
+console.log("================================================================");
+
+// Stats overview
+// --------------------------------------
+function format (val){
+    return Math.round(((val / 1024 / 1024) * 1000) / 1000) + 'mb';
+}
+
+var statsId = setInterval(function () {
+    console.log('Memory Usage :: '.bold.green +
+        ("\tRSS: " + format(process.memoryUsage().rss)).blue +
+        ("\tHeap Total: " + format(process.memoryUsage().heapTotal)).red +
+        ("\t\tHeap Used: " + format(process.memoryUsage().heapUsed)).magenta
+    );
+}, 1000);
+
+// Websocket server
+// --------------------------------------
 var numClients = 0;
-
-console.log("Server starting");
-
-wsServer.on('connection', function(ws) {
+wsServer.on('connection', function (ws) {
     numClients++;
     console.log("Client connected! : ", numClients);
 
-    ws.on('message', function(message) {
+    ws.on('message', function (message) {
         console.log('received message: %s', message);
+    });
+
+    ws.on('close', function () {
+        numClients--;
+        console.log('Disconnected : ', numClients);
     });
 
     //// Send a message...
