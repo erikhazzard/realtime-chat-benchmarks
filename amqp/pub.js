ccec79f1e50cff98a1bc79049d5d15be520fbc6b
Erik Hazzard <enoex1@gmail.com>
2014-07-08 16:34:01 -0700

    adds some pub/sub tests for rabbitmq

---
+++ amqp/pub.js
@@ -0,0 +1,29 @@
+/* =========================================================================
+ *
+ * pub.js 
+ *  publishes messages
+ *
+ * ========================================================================= */
+var colors = require('colors');
+var amqp = require('amqp');
+var connection = amqp.createConnection({ host: 'localhost' });
+
+connection.on('error', function(err) {
+    if((err+'').match('socket is closed')){
+        console.log('Could not connect to AMQP (rabbitmq-server is not running)');
+    }
+    console.log('Error: ' + (err+'').red);
+});
+
+connection.on('ready', function(err) {
+    //catch redis errors so server doesn't blow up
+    console.log('AMQP Server connection established!'.green);
+
+    setInterval(function(){
+        connection.publish(
+            'pubsub',
+            { message: "Hello", token: "test" }, 
+            { contentType: 'application/json', contentEncoding: 'utf-8' }
+        );
+    });
+});
