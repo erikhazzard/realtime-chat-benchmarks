3bf3f86a1d3526ee66ff7495a1778debbc9a57eb
Nils Schlomann <nils@visual.ly>
2014-07-09 00:24:40 -0700

    Adds more docu

---
+++ mqtt/test-rooms-2/date_diff.sh
@@ -0,0 +1,14 @@
+ #!/bin/bash          
+echo "Starting script"
+
+lastDate=$(tail -n1 ./logs/logs.log|cut -f3 -d$'\t')
+firstDate=$(head -n1 ./logs/logs.log|cut -f3 -d$'\t')
+ 
+#echo $firstDate
+#echo $lastDate
+
+timeInMs=$(expr $lastDate - $firstDate)
+timeInSec=$(expr $timeInMs / 100)
+
+echo "Broadcasting messages took $timeInMs ms"
+
