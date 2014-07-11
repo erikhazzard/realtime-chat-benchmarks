51f7a1ae2336c72660d7e9e1afb57ed69870ab20
Dennis Zhao <denniszhao@berkeley.edu>
2014-07-11 11:53:09 -0700

    Cleans up some of the code and removes unnecessary files in websockets folder

--- websockets/ws/connections/client-cluster-connections.js
+++ websockets/ws/connections/client-cluster-connections.js
@@ -1,31 +1,24 @@
 /* =========================================================================
  *
- * send-single-message
- *  establishes a single connection and sends a message
+ *  client-cluster-connections
+ *
+ *  Spawns a clustered bunch of connections that
  *
  *
  * ========================================================================= */
 
+var WebSocket = require('ws');
 var colors = require('colors');
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 var async = require('async');
 var _ = require('lodash');
+var logMemUsage = require('../../../util/mem-usage');
 
-// Stats overview
-// --------------------------------------
-function format (val){
-    return Math.round(((val / 1024 / 1024) * 1000) / 1000) + 'mb';
-}
+var NUM_CONNECTIONS = 10000;
 
 if (cluster.isMaster) {
-    var statsId = setInterval(function () {
-        console.log('Memory Usage :: '.bold.green.inverse +
-            ("\tRSS: " + format(process.memoryUsage().rss)).blue +
-            ("\tHeap Total: " + format(process.memoryUsage().heapTotal)).yellow +
-            ("\t\tHeap Used: " + format(process.memoryUsage().heapUsed)).magenta
-        );
-    }, 1500);
+    logMemUsage(1500);
 
     for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
@@ -36,55 +29,45 @@ if (cluster.isMaster) {
         console.log(('worker ' + worker.process.pid + ' died').red);
         console.log('xxxxxxxxxxxxxxxxxx'.bold.red);
     });
-
 } else {
-
     process.on('uncaughtException', function (err) {
         console.log('xxxxxxxxxxxxxxxxxx'.bold.yellow);
-        console.log('  UNCAUGHT EXCEPTION '.yellow);
-        console.log(err+'');
+        console.log('UNCAUGHT EXCEPTION'.yellow);
+        console.log(err + '');
         console.log('xxxxxxxxxxxxxxxxxx'.bold.yellow);
     });
 
     console.log(('Worker started! ' + cluster.worker.id).grey);
-    var numConnections = 0;
 
-    async.eachLimit(_.range(5000), 1000, function (i, cb){
-        var WebSocket = require('ws');
-        try{
+    async.eachLimit(_.range(NUM_CONNECTIONS), NUM_CONNECTIONS / 20, function (i, cb) {
+        try {
             var ws = new WebSocket('ws://localhost:3000/', {
-                protocolVersion: 8, 
+                protocolVersion: 8,
                 origin: 'http://localhost:3000'
             });
-            var colors = require('colors');
 
-            console.log(('Connecting : worker ' + cluster.worker.id + '... ('+i+')').grey);
+            console.log("Connecting #" + i + " on worker #" + cluster.worker.id);
 
             // Setup connections, open connection
-            ws.on('open', function () {
-                numConnections++;
-                console.log('Connected'.green.bold + ' worker: ' + 
-                    cluster.worker.id +
-                    ' | Num connections : ' + (''+numConnections).blue);
+            ws.on('open', function setupConnection() {
 
-                // continue
-                setTimeout(function(){
-                    console.log('\t calling callback... worker: ' + cluster.worker.id);
-                    cb();
-                }, 1000);
+                // continue, allow some time between establishing connections
+                setTimeout(cb, 1000);
             });
 
             ws.on('close', function () {
                 console.log('xxxxxxxxxxxxxxxxxx'.bold.red);
-                console.log('  Disconnected'.red);
+                console.log("Socket #" + i + " disconnected".red);
                 console.log('xxxxxxxxxxxxxxxxxx'.bold.red);
             });
-        } catch(err){
+        } catch(err) {
             console.log('xxxxxxxxxxxxxxxxxx'.bold.yellow);
-            console.log('  UNCAUGHT ERROR CONNECTING TO WEBSOCKET '.yellow);
+            console.log('UNCAUGHT ERROR CONNECTING TO WEBSOCKET '.yellow);
             console.log(err+'');
             console.log('xxxxxxxxxxxxxxxxxx'.bold.yellow);
         }
-
+    }, function onDoneConnecting() {
+        // all of the async calls have finished
+        console.log("Done connecting " + NUM_CONNECTIONS + " clients.");
     });
 }
