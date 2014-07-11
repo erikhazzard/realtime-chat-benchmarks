3c889de8c960fa6b8286363dba04e75fc603e93e
Dennis Zhao <denniszhao@berkeley.edu>
2014-07-11 15:58:03 -0700

    adds mqtt uri as command line arg for test-rooms-random-intervals

--- mqtt/test-rooms-random-intervals/client-profiling.js
+++ mqtt/test-rooms-random-intervals/client-profiling.js
@@ -3,7 +3,7 @@
  * Clients:
  *  Starts multiple clients asynchronously and connects all of them to a
  *  channel
- *  
+ *
  * After creating all clients. The script will send one message to each room
  * which will be received from every member of the room
  *
@@ -21,7 +21,8 @@ var mqtt = require('mqtt'),
     helpers = require('./helpers.js'),
     NUM_CLIENTS = process.argv[2] || 0,
     NUM_ITERATIONS = process.argv[3] || Infinity,
-    totalClients = 0;
+    totalClients = 0,
+    mqttUri = process.argv[4] || 'localhost';
 
 var DELAY_BETWEEN_CONCURRENT_SENDERS = 20;
 var sys = require('sys');
@@ -55,7 +56,7 @@ winston.info("Number of iterations: ", NUM_ITERATIONS);
 
 async.each(_.range(NUM_CLIENTS), function(i, callback) {
 
-    var client = mqtt.createClient(8883, 'localhost');
+    var client = mqtt.createClient(8883, mqttUri);
 
 
     if(index === 6){
@@ -89,7 +90,7 @@ async.each(_.range(NUM_CLIENTS), function(i, callback) {
 
 
     client.on("message", function(topic, message) {
-        
+
         var time = new Date().getTime();
         var timeSpent = time - parseInt(message, 10);
         var message = "Msg \t"+topic+"\t"+timeSpent+"\t"+this.clientId+"\n";
@@ -117,28 +118,28 @@ async.each(_.range(NUM_CLIENTS), function(i, callback) {
             client_iterations.push(0);
             setTimeout(function (index){
                 return function (){
-                    
-                    //Save the id of the interval 
+
+                    //Save the id of the interval
                     var timerId = setInterval(function (index_inner){
-                        
+
                             return function () {
-                                
+
 
                                 if(NUM_ITERATIONS < Infinity && client_iterations[index_inner] < NUM_ITERATIONS){
 
                                     var client = broadcastClients[index_inner];
                                     client.publish(client.roomId, new Date().getTime()+"");
-                                    client_iterations[index_inner] +=1; 
+                                    client_iterations[index_inner] +=1;
 
                                 } else {
-                                  
-                                  clearInterval(intervalIds[index_inner]);  
+
+                                  clearInterval(intervalIds[index_inner]);
 
                                   if(index_inner === intervalIds.length-1){
-                                    
+
                                     setTimeout(function(d, i){
-                                        
-                                        console.log("Finished. Time to run all iterations", (new Date() - startIntervals)/1000, "s"); 
+
+                                        console.log("Finished. Time to run all iterations", (new Date() - startIntervals)/1000, "s");
                                         var column = 2;
                                         helpers.calcAvg( logFilePath, column, function(val){
                                             console.log("Average MsgTime time (ms): " + val);
@@ -146,24 +147,24 @@ async.each(_.range(NUM_CLIENTS), function(i, callback) {
                                             helpers.calcAvg(logFilePathCpuAvg, 0, function(val){
                                                 console.log("Average CPU time: " + val.toFixed(2),'%');
                                                 console.log("Message Send Total: ", msgNr);
-                                                process.exit(0);    
+                                                process.exit(0);
                                             });
 
-                                            
+
                                        });
 
-                                        
-                                        
+
+
 
                                     }, 1000);
                                   }
 
                                 }
 
-                            }    
-                        
+                            }
 
-                        
+
+
 
                     }(index), DELAY_BETWEEN_CONCURRENT_SENDERS * NUM_CONCURRENT_SENDERS);
 
@@ -181,5 +182,5 @@ async.each(_.range(NUM_CLIENTS), function(i, callback) {
 
 
 
-    
+
 });
