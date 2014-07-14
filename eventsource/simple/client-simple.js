ad064e55aeed0ad0bc4e6b48546cc781699e552b
Erik Hazzard <enoex1@gmail.com>
2014-07-14 15:47:07 -0700

    adds some basic eventsource code

---
+++ eventsource/simple/client-simple.js
@@ -0,0 +1,19 @@
+/* =========================================================================
+ * 
+ * client-simple.js
+ *  Simple eventsource client
+ *
+ *  ======================================================================== */
+var EventSource = require('eventsource');
+
+var es = new EventSource('http://localhost:8010/eventsource');
+//var es = new EventSource('http://127.0.0.1:8010/sse');
+
+// Message callbacks
+es.onmessage = function(e) {
+    console.log('Got message :::', e);
+    console.log(e.data);
+};
+es.onerror = function() {
+    console.log('ERROR!');
+};
