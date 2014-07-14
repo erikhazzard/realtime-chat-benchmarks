94a608ceb681181d6b5ca192a33446d267fb01e6
roundrobin <nils@five.com>
2014-07-14 16:43:47 -0700

    Adds client for multiple connections

--- eventsource/client.js
+++ eventsource/client.js
@@ -1,7 +1,16 @@
 var EventSource = require('eventsource');
-var es = new EventSource('http://localhost:8080/sse');
+var es = new EventSource('http://localhost:1223/sse');
 
 
 es.onmessage = function(e) {
   console.log(e.data);
 };
+
+es.onerror = function(e) {
+  console.log("ES:: Error:: ", e.data);
+};
+
+es.onopen = function(e) {
+  console.log("ES:: Openend Connection:: ",e);
+};
+
--- eventsource/server.js
+++ eventsource/server.js
@@ -1,14 +1,24 @@
-var SSE = require('sse')
+var SSE = require('sse');
   , http = require('http');
 
 var server = http.createServer(function(req, res) {
+  console.log("Client connected", req.headers)
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('okay');
 });
 
-server.listen(8080, '127.0.0.1', function() {
+server.listen(1223, '127.0.0.1', function() {
   var sse = new SSE(server);
+
   sse.on('connection', function(client) {
+    console.log("Client connected");
     client.send('hi there!');
   });
+
+
+  sse.on('error', function(client) {
+    console.log("Error");
+    
+  });
+
 });
