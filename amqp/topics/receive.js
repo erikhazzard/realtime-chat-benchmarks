b3db00051798e13b94cdba3e5a8288a31d748659
Erik Hazzard <enoex1@gmail.com>
2014-07-15 14:37:52 -0700

    adds some more amqp examples of topics / exchanges

---
+++ amqp/topics/receive.js
@@ -0,0 +1,22 @@
+var amqp = require('amqp');
+var connection = amqp.createConnection({host: 'localhost'});
+var message = "Hello " + (Math.random() * 100 | 0);
+var routing = '#';
+
+connection.on('ready', function(){
+
+    connection.exchange( 'topic_logs', {type: 'topic', autoDelete: false}, function(exchange){
+
+        connection.queue('tmp-' + Math.random(), {exclusive: true},
+            function(queue){
+                queue.bind('topic_logs', routing);
+                console.log('Waiting for messages on : ' + routing);
+
+                queue.subscribe(function(msg){
+                    console.log(" [x] %s", msg.data.toString('utf-8'));
+                });
+        });
+
+    });
+
+});
