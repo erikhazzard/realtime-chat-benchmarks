85ae6c0594ea1b10f75664821023a8fdd93df43b
roundrobin <nils@five.com>
2014-07-15 17:14:03 -0700

    Adds litle changes to event source server simple

--- eventsource/express/multiple-clients.js
+++ eventsource/express/multiple-clients.js
@@ -12,34 +12,38 @@ http.globalAgent.maxSockets = Infinity;
 
 
 var nrOfClients = process.argv[2] || 1;
+var port = process.argv[3] || 8010;
 
 console.log("Number of Clients: ", nrOfClients);
+console.log("Connect to port: ", port);
 
 async.eachLimit(_.range(nrOfClients), 20, function(index, callback){
   console.log("Create client", "index", index);
 
-  var es = new EventSource('http://localhost:8010/eventsource');
+  var es = new EventSource('http://localhost:'+(port)+'/eventsource');
   //var es = new EventSource('http://127.0.0.1:8010/sse');
 
   // Message callbacks
   es.onmessage = function(i){
     return function(e) {
-        console.log('Got message :::', i, e.data);
+        //console.log('Got message :::', i, e.data);
         //console.log(e.data);
     };
   }(index);
   
-  es.onerror = function() {
-      console.log('ERROR!');
+  es.onerror = function(e) {
+      console.log('ERROR!', JSON.stringify(e));
   };
 
   
   es.addEventListener("room-1", function(e){
-    console.log('Got message :::', e.data);
+    //console.log('Got message :::', e.data);
   });
 
 
-  callback();
+  process.nextTick(function(d, i){
+    callback();
+  });
 
   
 
